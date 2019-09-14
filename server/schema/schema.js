aea073448262bcb6c3d301c6e471a99ad9775d72
Gabriel Stratan <contact@ithanium.org>
2019-09-14 23:16:30 +0100

    initial commit

---
+++ server/schema/schema.js
@@ -0,0 +1,30 @@
+const graphql = require('graphql');
+
+const{GraphQLObjectType, GraphQLString, GraphQLSchema} = graphq l;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString},
+    name: { type: GraphQLString},
+    genre: { type: GraphQLString}
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {id: {type: GraphQLString}},
+      resolve(parent, args){
+        // code to get data from db / other source
+        // args.id
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
